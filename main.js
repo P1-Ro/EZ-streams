d7ae9f468c33d1832d7ffc3b17a27070f49c057d
Jozef Dochan <theglow666@gmail.com>
2018-02-10 18:08:34 +0100

    updated

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
-const electron = require('electron');
+const { Menu, app, BrowserWindow} = require('electron');
 const {autoUpdater} = require("electron-updater");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
 const fs = require('fs');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+let flagStore = {};
+
+autoUpdater.on('update-downloaded', () => {
+    fs.unlinkSync(flagStore.cwd + "cache.db");
+});
+
+function setCWD() {
+    flagStore = {cwd: "backend/dist/app/"};
+    flagStore.DEBUG = true;
+    if (fs.existsSync("resources/app")) {
+        flagStore = {cwd: "resources/app/"};
+        flagStore.DEBUG = false;
+    }
+}
 
 function createWindow() {
 
     autoUpdater.checkForUpdatesAndNotify();
     let process_name = "app.exe";
-    let DEBUG = true;
-    let cwd = {cwd: "backend/dist/app"};
-    if (fs.existsSync("resources/app")) {
-        cwd = {cwd: "resources/app/"};
-        DEBUG = false;
-    }
+    setCWD();
 
 
-    let subpy = require('child_process').spawn(process_name, cwd);
+    let subpy = require('child_process').spawn(process_name, flagStore);
     let rq = require('request-promise');
     let mainAddr = 'http://localhost:8080';
 
@@ -30,10 +37,15 @@ function createWindow() {
                 nodeIntegration: false
             }
         });
+        mainWindow.maximize();
         mainWindow.loadURL(mainAddr);
-        if(DEBUG){
+        if (flagStore.DEBUG) {
             mainWindow.webContents.openDevTools();
         }
+        mainWindow.webContents.on('new-window', function (event) {
+
+            event.preventDefault();
+        });
         mainWindow.on('closed', function () {
             mainWindow = null;
             subpy.kill('SIGINT');
@@ -55,9 +67,46 @@ function createWindow() {
     startUp();
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+const template = [
+    {
+        label: "Tools",
+        submenu: [
+            {
+                label: "Clear cache",
+                click() {
+                    if(fs.existsSync(flagStore.cwd + "cache.db")){
+                        fs.unlinkSync(flagStore.cwd + "cache.db");
+                    }
+                        mainWindow.webContents.session.clearStorageData();
+                        mainWindow.webContents.reload();
+                }
+            },
+        ]
+    },
+    {
+        label: "Help",
+        submenu: [
+            {
+                label: "Open developer tools",
+                click() {
+                    mainWindow.webContents.openDevTools();
+                }
+            },
+            {
+                label: "See on GitHub",
+                click() {
+                    require('electron').shell.openExternal('https://github.com/P1-Ro/EZ-streams')
+                }
+            },
+            {
+                label: "About",
+            },
+        ]
+    },
+];
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
@@ -76,6 +125,3 @@ app.on('activate', function () {
         createWindow()
     }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
