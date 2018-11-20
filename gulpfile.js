0cd0e17049daf1a00867a909675a2ddb27941c43
Jozef Dochan <theglow666@gmail.com>
2018-11-20 22:53:34 +0100

    clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var run = require('gulp-run-command').default
+var run = require('gulp-run-command').default;
 
 gulp.task('build_backend', run('python ./freeze.py', {
     cwd: "./backend/"
@@ -7,8 +7,4 @@ gulp.task('build_backend', run('python ./freeze.py', {
 
 gulp.task('deploy', ['build_backend'], run('release.bat'));
 
-gulp.task('start_backend', run('pipenv run python app.py', {
-    cwd: "./backend/"
-}));
 gulp.task('start_client', run("npm start"));
-gulp.task('start', ['start_backend', 'start_client'] );
